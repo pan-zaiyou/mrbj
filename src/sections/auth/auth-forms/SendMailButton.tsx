71b20e6ba0ca569d36b599cccc8e31e7c984afcb
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2024-07-21 12:54:26 +0800

    Update SendMailButton.tsx

--- src/sections/auth/auth-forms/SendMailButton.tsx
+++ src/sections/auth/auth-forms/SendMailButton.tsx
@@ -1,67 +1,36 @@
-import React, { useState } from "react";
+
+import React from "react";
 import lo from "lodash-es";
 
-// Material-UI
+// material-ui
 import { Dialog, DialogContent, DialogTitle, InputAdornment } from "@mui/material";
 import { useSnackbar } from "notistack";
-import IconButton from "@/components/@extended/IconButton";
-import { SendOutlined } from "@ant-design/icons";
 
-// Third Party
+// third party
 import ReCaptcha from "react-google-recaptcha";
 import { Trans, useTranslation } from "react-i18next";
-import ReactGA from "react-ga4";
 
-// Project Imports
+// project import
+import IconButton from "@/components/@extended/IconButton";
 import { useGetGuestConfigQuery, useSendEmailVerifyMutation } from "@/store/services/api";
 
-// Component Definition
+// assets
+import { SendOutlined } from "@ant-design/icons";
+import ReactGA from "react-ga4";
+
+// ============================|| AUTH - SEND EMAIL VERIFY ||============================ //
+
 export interface SendMailButtonProps {
   email: string;
 }
 
-const SendMailWithCaptchaButton: React.FC<SendMailButtonProps> = ({ email }) => {
+export const SendMailWithCaptchaButton: React.FC<SendMailButtonProps> = ({ email }) => {
   const { data: guestConfig } = useGetGuestConfigQuery();
   const [sendEmailVerify, { isLoading }] = useSendEmailVerifyMutation();
   const { enqueueSnackbar } = useSnackbar();
   const { t } = useTranslation();
 
-  const [open, setOpen] = useState(false);
-
-  const handleCaptchaVerify = (token: string | null) => {
-    if (lo.isNull(token)) {
-      enqueueSnackbar(t("auth.captcha.error_null_token"), { variant: "error" });
-      return;
-    }
-
-    sendEmailVerify({ email, recaptcha_data: token })
-      .unwrap()
-      .then(() => {
-        enqueueSnackbar(t("auth.captcha.success"), { variant: "success" });
-        ReactGA.event({
-          category: "auth",
-          action: "send_email_verify",
-          label: "send_email_verify",
-          email,
-          success: true,
-        });
-      })
-      .catch((err) => {
-        console.error(err);
-        enqueueSnackbar(t("auth.captcha.error"), { variant: "error" });
-        ReactGA.event({
-          category: "auth",
-          action: "send_email_verify",
-          label: "send_email_verify",
-          email,
-          success: false,
-          error: err.toString(),
-        });
-      })
-      .finally(() => {
-        setOpen(false);
-      });
-  };
+  const [open, setOpen] = React.useState(false);
 
   return (
     <>
@@ -83,7 +52,38 @@ const SendMailWithCaptchaButton: React.FC<SendMailButtonProps> = ({ email }) => 
         <DialogContent>
           <ReCaptcha
             sitekey={guestConfig?.recaptcha_site_key!}
-            onChange={handleCaptchaVerify}
+            onChange={(token: string | null) => {
+              if (lo.isNull(token)) {
+                enqueueSnackbar(t("auth.captcha.error_null_token"), { variant: "error" });
+                return;
+              }
+
+              sendEmailVerify({ email, recaptcha_data: token! })
+                .unwrap()
+                .then(() => {
+                  enqueueSnackbar(t("auth.captcha.success"), { variant: "success" });
+                  ReactGA.event("send_email_verify", {
+                    category: "auth",
+                    label: "send_email_verify",
+                    email: email,
+                    success: true
+                  });
+                })
+                .catch((err) => {
+                  console.error(err);
+                  enqueueSnackbar(t("auth.captcha.error"), { variant: "error" });
+                  ReactGA.event("send_email_verify", {
+                    category: "auth",
+                    label: "send_email_verify",
+                    email: email,
+                    success: false,
+                    error: err
+                  });
+                })
+                .finally(() => {
+                  setOpen(false);
+                });
+            }}
           />
         </DialogContent>
       </Dialog>
@@ -110,25 +110,23 @@ const SendMailButton: React.FC<SendMailButtonProps> = ({ email }) => {
       });
   };
 
-  return (
-    <>
-      {siteConfig?.is_recaptcha === 1 ? (
-        <SendMailWithCaptchaButton email={email} />
-      ) : (
-        <InputAdornment position="end">
-          <IconButton
-            aria-label="send email code"
-            onClick={handleSendEmailCode}
-            edge="end"
-            color="secondary"
-            disabled={isLoading}
-          >
-            <SendOutlined />
-          </IconButton>
-        </InputAdornment>
-      )}
-    </>
-  );
+  if (siteConfig?.is_recaptcha === 1) {
+    return <SendMailWithCaptchaButton email={email} />;
+  } else {
+    return (
+      <InputAdornment position="end">
+        <IconButton
+          aria-label="send email code"
+          onClick={handleSendEmailCode}
+          edge="end"
+          color="secondary"
+          disabled={isLoading}
+        >
+          <SendOutlined />
+        </IconButton>
+      </InputAdornment>
+    );
+  }
 };
 
 export default SendMailButton;
