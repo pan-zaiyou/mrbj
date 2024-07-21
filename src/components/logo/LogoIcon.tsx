9c164173d8c66e93ece4f2290dc88a20703a0efc
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2024-07-22 00:20:51 +0800

    Update LogoIcon.tsx

--- src/components/logo/LogoIcon.tsx
+++ src/components/logo/LogoIcon.tsx
@@ -1,13 +1,1 @@
-import { useTheme } from '@mui/material/styles';
 
-// ==============================|| LOGO ICON SVG ||============================== //
-
-const LogoIcon = () => {
-  const theme = useTheme();
-
-  return (
-    
-  );
-};
-
-export default LogoIcon;
