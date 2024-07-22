67bc59d6f7a839b8712ba21dd5d78580ec529bcb
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2024-07-22 13:18:36 +0800

    Update forgot-password.tsx

--- src/pages/auth/forgot-password.tsx
+++ src/pages/auth/forgot-password.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import { Trans } from "react-i18next";
 import { Link } from "react-router-dom";
-
-// material-ui
 import { Grid, Stack, Typography } from "@mui/material";
-
-// project import
 import AuthWrapper from "@/sections/auth/AuthWrapper";
 import AuthForgotPassword from "@/sections/auth/auth-forms/AuthForgotPassword";
 import useTitle from "@/hooks/useTitle";
-
-// ================================|| FORGOT PASSWORD ||================================ //
 
 const ForgotPassword: React.FC = () => {
   useTitle("forgot-password");
@@ -26,10 +20,15 @@ const ForgotPassword: React.FC = () => {
             sx={{ mb: { xs: -0.5, sm: 0.5 } }}
           >
             <Typography variant="h3">
-              <Trans i18nKey={"forgot_password.title"}>Forgot password</Trans>
+              <Trans i18nKey="forgot_password.title">Forgot password</Trans>
             </Typography>
-            <Typography component={Link} to={"/login"} variant="body1" sx={{ textDecoration: "none" }} color="primary">
-              <Trans i18nKey={"forgot_password.back_to_login"}>Back to login</Trans>
+            <Typography
+              component={Link}
+              to="/login"
+              variant="body1"
+              sx={{ textDecoration: "none", color: "primary.main" }}
+            >
+              <Trans i18nKey="forgot_password.back_to_login">Back to login</Trans>
             </Typography>
           </Stack>
         </Grid>
