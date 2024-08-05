bb34679c7b2b76b589bc0f15fbb81d1a073b8311
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2024-08-05 14:04:21 +0800

    Delete src/custom.ts

--- src/custom.ts
+++
@@ -1,7 +0,0 @@
-// src/custom.ts
-
-export const handleMobileClick = () => {
-    if (navigator.userAgent.match(/(iPhone|iPod|Android|ios|iOS|iPad|Blackberry|WebOS|Symbian|Windows Phone|Phone)/i)) {
-        document.body.classList.remove('sidebar-open');
-    }
-};
