b1a35c46345d3d435ba8600f5a1fffbdc4fd78d7
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2024-08-05 13:17:19 +0800

    Update custom.ts

--- src/custom.ts
+++ src/custom.ts
@@ -1,4 +1,4 @@
-// src/custom.js
+// src/custom.ts
 
 export const handleMobileClick = () => {
     if (navigator.userAgent.match(/(iPhone|iPod|Android|ios|iOS|iPad|Blackberry|WebOS|Symbian|Windows Phone|Phone)/i)) {
