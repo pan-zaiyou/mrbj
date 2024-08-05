e1eb33f015e34f216b836483c3cd016a9f4954db
onlyou <58397297+pan-zaiyou@users.noreply.github.com>
2024-08-05 13:12:30 +0800

    Add files via upload

---
+++ src/custom.js
@@ -0,0 +1,7 @@
+// src/custom.js
+
+export const handleMobileClick = () => {
+    if (navigator.userAgent.match(/(iPhone|iPod|Android|ios|iOS|iPad|Blackberry|WebOS|Symbian|Windows Phone|Phone)/i)) {
+        document.body.classList.remove('sidebar-open');
+    }
+};
